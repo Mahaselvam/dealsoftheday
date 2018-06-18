f28e353134607b29e051eeb30c6fb1dc2d8293ae
palani <shpalani@lm-maa-26500454.paypalcorp.com>
2018-06-18 13:38:18 +0530

    Flipkart deals of the day

---
+++ routes/index.js
@@ -0,0 +1,55 @@
+var express = require('express'),
+	request = require('request'),
+	http = require('http'),
+	router = express.Router(),
+  path = require('path'),
+	aff = require('flipkart-affiliate');
+
+var config =require('../config/config');
+
+var fkc = aff.createClient({
+  FkAffId: config.flipkart.id,
+  FkAffToken: config.flipkart.token,
+  responseType: 'json'
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.render('index', {
+		title : 'Express'
+	});
+});
+router.get('/home', function(req, res, next) {
+	res.render('home', {
+		title : 'Express'
+	});
+});
+
+router.post('/store/search', function(req, res, next) {
+
+	var store = req.body.store;
+	if(store =='flipkart'){
+		fkc.getDealsOfDay(null,function(error, response){
+		  if(error){
+				console.log('Error:', error);
+				res.send({
+					flag : false,
+					data : error
+				});
+		  }else{
+				res.send({
+					flag : true,
+					data : JSON.parse(response)
+				});
+		  }
+		});
+	}else {
+		res.send({
+			flag : false,
+			store: 'none'
+		});
+	}
+
+});
+
+module.exports = router;
